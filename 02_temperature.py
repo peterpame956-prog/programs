d644942fd407f56cdee7d128a8cd284c7786b1af
peterpame956-prog <peterpame956@gmail.com>
2026-06-18 16:34:24 +0800

    Correct Fahrenheit formula

--- 02_temperature.py
+++ 02_temperature.py
@@ -3,7 +3,7 @@
 # You will fix this on a feature branch, then merge the branch into main.
 
 def celsius_to_fahrenheit(c):
-    return c * 9 / 5      # wrong: should be c * 9 / 5 + 32
+    return c * 9 / 5 + 32      # wrong: should be c * 9 / 5 + 32
 
 print(celsius_to_fahrenheit(0))     # prints 0.0, but should be 32.0
 print(celsius_to_fahrenheit(100))   # prints 180.0, but should be 212.0
