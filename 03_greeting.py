7685f00a742bb34c268744a66d11fb8d2863895e
peterpame956-prog <peterpame956@gmail.com>
2026-06-18 17:28:31 +0800

    Remove files

--- 03_greeting.py
+++
@@ -1,9 +0,0 @@
-# Greeting program - Exercise 3 starting point
-# PROBLEM: two teammates want to change the greeting message in different ways.
-# You will edit the SAME line on two branches to create a merge conflict,
-# then resolve it.
-
-def greet(name):
-    return "Hello, " + name
-
-print(greet("World"))
--- 10_stats.py
+++
@@ -1,9 +0,0 @@
-# Stats helper - Exercise 10 starting point
-# PROBLEM: you will add a new function, commit it, then accidentally throw the
-# commit away with 'git reset --hard'. You will recover the lost commit using
-# 'git reflog' and 'git reset'.
-
-def mean(numbers):
-    return sum(numbers) / len(numbers)
-
-print(mean([2, 4, 6]))   # 4.0
